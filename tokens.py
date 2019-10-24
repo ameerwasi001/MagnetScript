425859d131bf6fcaf43dd44a9ca0f36c150298ce
ameerwasi001 <54429413+ameerwasi001@users.noreply.github.com>
2019-10-24 06:13:06 -0700

    Update tokens.py

--- tokens.py
+++ tokens.py
@@ -7,6 +7,8 @@ def tokenize(content):
     content = content.replace('when ', 'while ')
     content = content.replace('when(', 'while(')
     content = re.sub(r"\bunless \b","if not ",content)
+
+    #It should remain last in order otherwise many words with strings will break
     content = content.replace('{[', '')
     content = content.replace(']}', '')
     return content
