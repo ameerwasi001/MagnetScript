f13130cef60c01f05db5eb058773413483be69aa
ameerwasi001 <54429413+ameerwasi001@users.noreply.github.com>
2019-10-23 23:28:11 -0700

    Add files via upload

---
+++ tokens.py
@@ -0,0 +1,7 @@
+def tokenize(content):
+    content = content.replace('function ', 'def ')
+    content = content.replace('iterate ', 'for ')
+    content = content.replace('inside ', 'in ')
+    content = content.replace('when ', 'while ')
+    content = content.replace('when(', 'while(')
+    return content
