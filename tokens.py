1edf03889e879ca9da791e66669478b64984d2bd
ameerwasi001 <54429413+ameerwasi001@users.noreply.github.com>
2019-10-24 11:06:32 -0700

    Update tokens.py

--- tokens.py
+++ tokens.py
@@ -4,7 +4,7 @@ def tokenize(content):
     content = re.sub(r"\bfunction \b","def ",content)
     content = re.sub(r"\biterate \b","for ",content)
     content = re.sub(r"\binside \b","in ",content)
-    content = content.replace('when ', 'while ')
+    content = content.replace('when (', 'while (')
     content = content.replace('when(', 'while(')
     content = re.sub(r"\bunless \b","if not ",content)
 
