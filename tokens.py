6c22756ecf95d728bee3c874c16ca4dcce70a767
ameerwasi001 <54429413+ameerwasi001@users.noreply.github.com>
2019-11-03 03:09:54 -0800

    Update tokens.py

--- tokens.py
+++ tokens.py
@@ -1,6 +1,6 @@
 import re
 
-def tokenize(content):
+def tokenize(content, directory='./'):
     content = re.sub(r"\bfunction \b","def ",content)
     content = re.sub(r"\biterate \b","for ",content)
     content = re.sub(r"\binside \b","in ",content)
@@ -9,7 +9,9 @@ def tokenize(content):
     content = re.sub(r"\bunless \b","if not ",content)
     content = content.replace('>| ', 'lambda ')
     content = content.replace('>: ', ':\t')
-
+    if re.search('mgs_require\([^\)]*\)(\.[^\)]*\))?', content) is not None or re.search('require\([^\)]*\)(\.[^\)]*\))?', content) is not None:
+        content = content.replace('./', '{}/'.format(directory))
+        
     #It should remain last in order otherwise many words within strings will break
     content = content.replace('{[', '')
     content = content.replace(']}', '')
