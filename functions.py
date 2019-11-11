0af89f0a09d8e337b17c17b0f487b041935fc87f
ameerwasi001 <54429413+ameerwasi001@users.noreply.github.com>
2019-11-11 07:15:41 -0800

    Update functions.py

--- functions.py
+++ functions.py
@@ -155,7 +155,7 @@ def heatmap_image(image, data=False, cmap='coolwarm', vmin=None, vmax=None, cent
     plt.show()
 
 
-def bright_scale(image, data=False, outer_circle=False, dotted_lines=True, figsize=[6,4], cmap='gray'):
+def bright_scale(image, data=False, outer_circle=False, dotted_lines=True, figsize=[6,4], cmap='gray', output=True):
     if not data:
         image = imread(image)
     image[:10] = 0
@@ -172,11 +172,13 @@ def bright_scale(image, data=False, outer_circle=False, dotted_lines=True, figsi
         outer_disk_mask = (X - l_x / 2)**2 + (Y - l_y / 2)**2 > (l_x / 2)**2
         image[outer_disk_mask] = 0
 
-    plt.figure(figsize=(figsize[0], figsize[1]))
-    plt.imshow(image, cmap=cmap)
-    plt.axis('off')
-    plt.show()
-
+    if output:
+        plt.figure(figsize=(figsize[0], figsize[1]))
+        plt.imshow(image, cmap=cmap)
+        plt.axis('off')
+        plt.show()
+    else:
+        return image
 
 #Contrast a image with MagnetScript (Parameters aren't yet configured well)
 def washtred_image(image, data=False, output=True, cmap='nipy_spectral', figsize=[6,4]):
