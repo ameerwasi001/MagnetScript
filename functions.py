7da7d255905d63de90ac3e90ac75a326c21ffad9
ameerwasi001 <54429413+ameerwasi001@users.noreply.github.com>
2019-11-17 07:03:23 -0800

    Update functions.py

--- functions.py
+++ functions.py
@@ -5,6 +5,7 @@ from magpylib import source, Collection
 from magpylib.source import *
 import magpylib.source.magnet as magnet
 import matplotlib.pyplot as plt
+from matplotlib.colors import *
 import magpylib as magpy
 
 from scipy import ndimage as ndi
@@ -264,7 +265,9 @@ def watershed_image(image, data=False, output=True, interpolation="nearest", cma
 
 
 #Show image with MagnetScript
-def image_show(image, cmap='viridis', interpolation='nearest', alpha=1, vmin=None, vmax=None, filter_radius=4.0, figsize=[6,4], title='Image'):
+def image_show(image, data=True, cmap='viridis', interpolation='nearest', alpha=1, vmin=None, vmax=None, filter_radius=4.0, figsize=[6,4], title='Image'):
+    if not data:
+        image = imread(image)
     # display results
     fig, axes = plt.subplots(nrows=1, ncols=1, figsize=(figsize[0], figsize[1]), sharex=True, sharey=True, squeeze=False)
     ax = axes.ravel()
